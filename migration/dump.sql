0c2398a7d2d95c4b57cdd86618592e1fab4d3f56
Salma WADOUACHI <s_wadouachi@hetic.eu>
2023-01-10 11:32:57 +0100

    ajout table Payments

--- migration/dump.sql
+++ migration/dump.sql
@@ -1,11 +1,19 @@
 CREATE TABLE IF NOT EXISTS User
 (
-    id        INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_User      INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username  VARCHAR(255) NOT NULL,
     password  VARCHAR(255) NOT NULL,
     email     VARCHAR(255) NOT NULL,
     firstName VARCHAR(255),
     lastName  VARCHAR(255),
-    gender    CHAR(1),
-    roles     JSON         NOT NULL
+    gender    CHAR(1)
+);
+
+CREATE TABLE IF NOT EXISTS Payments
+(
+    id_Payment INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    date DATETIME,
+    description TEXT,
+    UserId INT NOT NULL,
+    CONSTRAINT FK_PaymentsUser FOREIGN KEY (UserId) REFERENCES User(id_User)
 );
